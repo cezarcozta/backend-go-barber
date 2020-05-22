6bda01de0363698ed176f0de6912ea341081c830
César Augusto Costa <cezarcozta@gmail.com>
2020-05-22 03:26:14 -0300

    feat: reste/forgot pwd

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -2,8 +2,8 @@ import { Router } from 'express';
 import multer from 'multer';
 import uploadConfig from '@config/upload';
 
-import UsersController from '../../controllers/UsersController';
-import UserAvatarController from '../../controllers/UserAvatarController';
+import UsersController from '../controllers/UsersController';
+import UserAvatarController from '../controllers/UserAvatarController';
 
 const usersRouter = Router();
 const usersControlller = new UsersController();
--- src/modules/users/infra/typeorm/repositories/UsersRepository.ts
+++ src/modules/users/infra/typeorm/repositories/UsersRepository.ts
@@ -21,8 +21,9 @@ class UsersRepository implements IUsersRepository {
   }
 
   public async findById(id: string): Promise<User | undefined> {
-    const user = await this.ormRepository.findOne(id);
-
+    const user = await this.ormRepository.findOne({
+      where: { id },
+    });
     return user;
   }
 

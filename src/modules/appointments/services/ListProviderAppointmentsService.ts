265e5347ec08ead25694d4c665fcae02985a7b7f
César Augusto Costa <cezarcozta@gmail.com>
2020-06-02 07:41:05 -0300

    feat: adjust frontend

--- src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -1,5 +1,5 @@
 import { inject, injectable } from 'tsyringe';
-// import { getHours, isAfter } from 'date-fns';
+import { classToClass } from 'class-transformer';
 
 import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
@@ -45,7 +45,7 @@ class ListProviderAppointmentsService {
       );
     }
 
-    await this.cacheProvider.save(cacheKey, appointments);
+    await this.cacheProvider.save(cacheKey, classToClass(appointments));
 
     return appointments;
   }
